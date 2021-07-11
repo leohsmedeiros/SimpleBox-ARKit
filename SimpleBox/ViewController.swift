a98b0d4eb2ad88e27cd8e037beff2097a214ae7b
Leonardo Medeiros <>
2021-07-11 15:26:03 -0300

    added gesture detection

--- SimpleBox/ViewController.swift
+++ SimpleBox/ViewController.swift
@@ -28,16 +28,26 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the scene to the view
         sceneView.scene = scene
         
+        /* 3D Elements of the scene */
+
         // width and height are in meters
         let box = SCNBox(width: 0.3, height: 0.3, length: 0.3, chamferRadius: 0)
         let text = SCNText(string: "Hello ARKIT!", extrusionDepth: 1.0)
         let sphere = SCNSphere(radius: 0.1)
 
+        /* Defining Materials */
+
         // all the sides of the box will have the same material
-        box.firstMaterial?.diffuse.contents = UIImage(named: "wood.jpeg")
+        let boxMaterial = SCNMaterial()
+        boxMaterial.name = "Color"
+        boxMaterial.diffuse.contents = UIImage(named: "wood.jpeg")
+        
+        box.firstMaterial = boxMaterial
         text.firstMaterial?.diffuse.contents = UIColor.blue
         sphere.firstMaterial?.diffuse.contents = UIImage(named: "earth.jpg")
 
+        /* Defining Nodes (scale, position) */
+        
         let boxNode = SCNNode(geometry: box)
         boxNode.position = SCNVector3(0, 0, -1)
 
@@ -48,10 +58,33 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let sphereNode = SCNNode(geometry: sphere)
         sphereNode.position = SCNVector3(0, 0.35, -1)
 
+        /*  */
+
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        
+        /* Adding into the scene */
+
         self.sceneView.scene.rootNode.addChildNode(boxNode)
         boxNode.addChildNode(textNode)
 
         self.sceneView.scene.rootNode.addChildNode(sphereNode)
+        
+        
+    }
+    
+    @objc func tapped(recognizer: UIGestureRecognizer) {
+        let sceneView = recognizer.view as! SCNView
+        let touchLocation = recognizer.location(in: sceneView)
+        let hitResults = sceneView.hitTest(touchLocation, options: [:])
+        
+        if !hitResults.isEmpty {
+            let node = hitResults[0].node
+            let material = node.geometry?.material(named: "Color")
+            
+            material?.diffuse.contents = UIColor.random()
+            
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
